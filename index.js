f7eda5a22b1684907ddb5a2bb0a6214c9c99f795
RamiroBertran <ramirobertra@gmail.com>
2023-06-24 18:04:21 -0300

    feat: add app.post() that check if the url is a bad or a good one, then it respond with a json object

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const url = require("url");
+const dns = require("dns");
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+app.post("/api/shorturl", (req, res) => {
+  const url_input = req.body.url_input;
+  console.log(url_input);
+  const parseUrl = url.parse(url_input);
+  if (parseUrl.hostname == null) {
+    res.status(400).json({ error: "invalid url" });
+  } else {
+    dns.lookup(parseUrl.hostname, (err, address) => {
+      if (err) {
+        console.error(err);
+        return;
+      } else {
+        res.json({ url: parseUrl.href });
+      }
+    });
+  }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
