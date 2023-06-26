e12e769d77d30fd39fc289518056af8076d56956
RamiroBertran <ramirobertra@gmail.com>
2023-06-26 14:24:17 -0300

    feat: given to condition to post app, which one check if value is null, and the other if the url is ok, then it send url to database

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+// url require('url') allows to call methods such as parse that will be need to check the url
 const url = require("url");
+// dns allows to use method such as dns.lookup to check again if the url is a well input
 const dns = require("dns");
+// bodyParser allows us to get the input from the user
 const bodyParser = require("body-parser");
+// uri contain the url to mongose database
+const uri = process.env.MONGO_URI;
+// Conect to monogose database
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// Create a mongoose schema
 
+const form_url = new mongoose.Schema({
+  url: String,
+  shorturl: Number,
+});
+
+// Create a mongoose model
+const User_url = mongoose.model("User_url", form_url);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -13,7 +29,7 @@ app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
@@ -26,6 +42,8 @@ app.post("/api/shorturl", (req, res) => {
   const url_input = req.body.url_input;
   // random_surl contain a random number between 1 and 10000 that will be asigned to a the url that was send trough the formulaire
   let random_surl = Math.round(Math.random() * 10000);
+  let number_to_string = `${random_surl}`;
+  console.log(typeof number_to_string);
   // parse url call the method parse trough url methods and keeping the value in parseUrl variable
   const parseUrl = url.parse(url_input);
   // if the hostname is null meaning that the url input in the form it's a wrong one
@@ -39,11 +57,20 @@ app.post("/api/shorturl", (req, res) => {
         console.error(err);
         return;
       } else {
-        // In case the two filter are passed then, it will respond with a json object that contain the entire url, and the shorturl, that is the random number given to the input url
-        const url_object = { url: parseUrl.href, shorturl: random_surl };
-        localStorage.setItem("url_shortUrl", JSON.stringify(url_object));
-        const storedObject = JSON.parse(localStorage.getITem("url_shortUrl"));
-        res.json(storedObject);
+        // In case the two filter are passed, then, it will respond with a json object that contain the entire url, and the shorturl, that is the random number given to the input url
+        const newUrl = new User_url({
+          url: parseUrl.href, 
+          shorturl:random_surl;  
+        });
+        newUrl.save((err) => {
+          if(err) {
+            console.error(err); 
+            console.log("An erro has ocurred"); 
+        } else {
+            console.log("User saved succesfully"); 
+        }
+        ); 
+        res.json(newUrl);
       }
     });
   }
