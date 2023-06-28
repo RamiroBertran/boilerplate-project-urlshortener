fd0b720202dde64e25a7e94d64cf6dc046a321d3
RamiroBertran <ramirobertra@gmail.com>
2023-06-27 22:59:26 -0300

    fix: all code made in this branch is new

--- index.js
+++ index.js
@@ -2,26 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require("mongoose");
-// url require('url') allows to call methods such as parse that will be need to check the url
-const url = require("url");
-// dns allows to use method such as dns.lookup to check again if the url is a well input
 const dns = require("dns");
-// bodyParser allows us to get the input from the user
+const url = require("url");
 const bodyParser = require("body-parser");
-// uri contain the url to mongose database
-const uri = process.env.MONGO_URI;
-// Conect to monogose database
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// Create a mongoose schema
+const mongoURL = process.env.MONGO_URI;
+const mongoose = require("mongoose");
 
-const form_url = new mongoose.Schema({
-  url: String,
-  shorturl: Number,
-});
-
-// Create a mongoose model
-const User_url = mongoose.model("User_url", form_url);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -35,47 +21,76 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-
-app.post("/api/shorturl", (req, res) => {
-  // url_input contain the value of the url input form
-  const url_input = req.body.url_input;
-  // random_surl contain a random number between 1 and 10000 that will be asigned to a the url that was send trough the formulaire
-  let random_surl = Math.round(Math.random() * 10000);
-  let number_to_string = `${random_surl}`;
-  console.log(typeof number_to_string);
-  // parse url call the method parse trough url methods and keeping the value in parseUrl variable
-  const parseUrl = url.parse(url_input);
-  // if the hostname is null meaning that the url input in the form it's a wrong one
-  if (parseUrl.hostname == null) {
-    // case it is null, return an status of 400 and a json object with a key of 'error' and a value of 'invalid url';
-    res.status(400).json({ error: "invalid url" });
-  } else {
-    // in case the pass the first filter it will be check trought the second filter wich verify using dns lookup method, giving to it the hostname, and then a call back function that console an error if there is a problem
-    dns.lookup(parseUrl.hostname, (err, address) => {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
-        // In case the two filter are passed, then, it will respond with a json object that contain the entire url, and the shorturl, that is the random number given to the input url
-        const newUrl = new User_url({
-          url: parseUrl.href, 
-          shorturl:random_surl;  
-        });
-        newUrl.save((err) => {
-          if(err) {
-            console.error(err); 
-            console.log("An erro has ocurred"); 
-        } else {
-            console.log("User saved succesfully"); 
-        }
-        ); 
-        res.json(newUrl);
-      }
-    });
-  }
+const urlSchema = mongoose.Schema({
+  url: String,
+  shorturl: String,
 });
 
+const urlModel = mongoose.model("urlModel", urlSchema);
+
+mongoose
+  .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database: " + error);
+  });
+
+const urlResponse = (res, url, shorturl) => {
+  res.json({ url: url, shorturl: shorturl });
+};
+app.post("/api/shorturl", (req, res) => {
+  // --- Required variables ----
+  const inputURL = req.body.url_input;
+  const parseURL = url.parse(inputURL);
+  const number = Math.round(Math.random() * 1000);
+  // ---  Check if hostname is null ---
+  if (parseURL.hostname === null) {
+    res.json({ error: "invalid url" });
+    return;
+  }
+  // --- Dns lookup method ---
+  dns.lookup(parseURL.hostname, (err, address) => {
+    if (err) {
+      console.error(err);
+      res.json({ error: "invalid url" });
+      return;
+    }
+  });
+  // -- Find out if the input url already in db --
+  urlModel.findOne({ url: inputURL }, (error, url) => {
+    if (error) {
+      console.error(error);
+    } else {
+      if (url == null) {
+        // -- Send url to database --
+        const newURL = new urlModel({ url: inputURL, shorturl: number });
+        newURL.save((err, url) => {
+          if (err) {
+            console.error(err);
+          } else {
+            urlResponse(res, inputURL, number);
+          }
+        });
+      } else {
+        urlResponse(res, url.url, url.shorturl);
+      }
+    }
+  });
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  urlModel.findOne({ shorturl: req.params.shorturl }, (err, url) => {
+    if (err) {
+      console.error(err);
+    } else if (url == null) {
+      console.log("null");
+    } else {
+      res.redirect(url.url);
+    }
+  });
+});
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
